b8bc54b13adc65e9546b757cdae701c5ff34abfe
sudeeksh <sudeekshnayak02gmail.com>
2026-02-08 10:39:46 +0000

    Process control block

---
+++ README.md
@@ -0,0 +1,1 @@
+#A simple linux shell
--- minishell.c
+++ minishell.c
@@ -2,6 +2,26 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+struct PCB{
+int pid;
+int ppid;
+int priority;
+};
+
 int main(){
-fork();
+struct PCB process;
+if(fork()){
+    process.pid=getpid();
+    process.ppid=getppid();
+    process.priority=1;
+}
+else{ process.pid=getpid();
+    process.ppid=getppid();
+    process.priority=1;}
+
+printf("------------------------------\n");
+printf("Process ID: %d\n",process.pid);
+printf("Process parent ID: %d\n",process.ppid);
+printf("Process priority number: %d\n",process.priority);
+printf("-------------------------------\n");
 return 0;}
